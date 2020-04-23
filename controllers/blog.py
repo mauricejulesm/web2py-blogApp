0538d17d4576b69d5d922d59e3e569ae08dce9f1
Maurice JM <julesmaurice.mulisa@gmail.com>
2020-04-24 00:09:18 +0200

    added update db records

--- controllers/blog.py
+++ controllers/blog.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 # try something like
 
+def index(): return dict(message="hello from blog.py")
+
+def update():
+    record = db.blog(request.args(0)) or redirect(URL('post'))
+    form = SQLFORM(db.blog, record)
+    if form.process().accepted:
+        response.flash = T('Record Updated')
+    else:
+        response.flash = T('Please complete the form.')
+    return locals()
+
 def display_form():
     form = SQLFORM(db.blog)
     if form.process().accepted:
@@ -10,4 +21,12 @@ def display_form():
         response.flash = 'Your form has shit errors'
     else:
         response.flash = 'You are new here, please fill out the damn form'
+    return locals()
+
+def thanks():
+    msg = "Thank you for submitting your post"
+    return locals()
+
+def post():
+    form = SQLFORM(db.blog).process()
     return locals()
